5bc87efa2444e998bacfbe19c3a490679abef306
noskoNeo <160015917+noskoNeo@users.noreply.github.com>
2025-04-22 12:16:34 +0200

    hui push von neo

---
+++ PlayfairConsole/Matrix.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayfairConsole;
+
+public class Matrix : SystemException
+{
+    static string Decrypt(string msg)
+    {
+        throw new NotImplementedException();
+    }
+    
+    static string Encrypt(string msg)
+    {
+        throw new NotImplementedException();
+    }
+}
