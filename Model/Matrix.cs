5b078e0120f3831b66bdc3c0a93df6377fc655bc
werwolfy <raphael.nigl@outlook.de>
2025-04-22 10:20:46 +0200

    [TASK] matrix1.0

--- Model/Matrix.cs
+++ Model/Matrix.cs
@@ -2,6 +2,13 @@
 
 public class Matrix : IChiffre
 {
+    public string Key { get; set; }
+    public char[,] Grid { get; set; }
+    public Matrix(string key)
+    {
+        Key = key.Replace('j', 'i');
+        Grid = GenerateMatrix(Key.ToCharArray());
+    }
     public string Encrypt(string msg)
     {
         throw new NotImplementedException();
@@ -10,5 +17,49 @@ public class Matrix : IChiffre
     public string Decrypt(string msg)
     {
         throw new NotImplementedException();
+    }
+
+    private char[,] GenerateMatrix(char[] key)
+    {
+        int alphabetCounter = 0;
+        char[] alphabet = GetAlphabet(key);
+        char[,] matrix = new char[5, 5];
+        for (int i = 0; i < 5; i++)
+        {
+
+            for (int j = 0; j < 5; j++)
+            {
+                if (j + i * 5 < key.Length)
+                {
+                    matrix[i, j] = key[j + i * 5];
+                }
+                else
+                {
+                    matrix[i, j] = alphabet[alphabetCounter];
+                    alphabetCounter++;
+                }
+          
+            }
+        }
+        return matrix;
+    }
+
+
+
+    private char[] GetAlphabet(char[] key)
+    {
+        List<char> alphabet = new List<char> {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+        for (int i = 0; i < key.Length; i++)
+        {
+            if (alphabet.Contains(key[i]))
+            {
+                alphabet.Remove(key[i]);
+            }
+        }
+        return alphabet.ToArray();
     }
 }
